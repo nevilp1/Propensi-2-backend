3a0d4f6fdc347b4b426e311f433068fc4bca1033
erikameliawati730 <erikameliawati730@gmail.com>
2021-05-20 13:20:52 +0700

    feat: add participant controller

--- src/main/java/propensi/Pin/Insight/controller/ParticipantRestController.java
+++ src/main/java/propensi/Pin/Insight/controller/ParticipantRestController.java
@@ -21,4 +21,19 @@ import java.util.NoSuchElementException;
 public class ParticipantRestController {
     @Autowired
     ParticipantRestService participantRestService;
+
+    @PostMapping(value="/participant/add")
+    private ParticipantModel createParticipant(@Valid @RequestBody ParticipantModel participant, BindingResult bindingResult){
+        if(bindingResult.hasFieldErrors()){
+            bindingResult
+                    .getFieldErrors()
+                    .stream()
+                    .forEach(f -> System.out.println(f.getField() + ": " + f.getDefaultMessage()));
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "Request body has invalid type or missing field"
+            );
+        }else{
+            return participantRestService.createParticipant(participant);
+        }
+    }
 }
