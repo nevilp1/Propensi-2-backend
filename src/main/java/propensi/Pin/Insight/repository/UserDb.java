9da147f14710eb73cf97fd286a1d121e0011a93f
Nevil Philia <nevilphiliapramuda@gmail.com>
2021-06-04 05:50:27 +0700

    fix backend

--- src/main/java/propensi/Pin/Insight/repository/UserDb.java
+++ src/main/java/propensi/Pin/Insight/repository/UserDb.java
@@ -19,8 +19,4 @@ public interface UserDb extends JpaRepository<UserModel, Long> {
     Boolean existsByEmail(String email);
     List<UserModel> findAll();
 
-//    @Query(value = "SELECT u.username FROM user_model u JOIN user_roles r on r.user_id = u.id JOIN role ro on ro.id = r.role_id where role_id = '2' or role_id = '3';", nativeQuery = true)
-//    List<PicAutocomplete> findPIC();
-
-
 }
