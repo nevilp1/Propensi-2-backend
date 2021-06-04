49a472323eb15a9c71567560f50610d07142b83d
Nevil Philia <nevilphiliapramuda@gmail.com>
2021-06-04 21:12:33 +0700

    change password type

--- src/main/java/propensi/Pin/Insight/model/UserModel.java
+++ src/main/java/propensi/Pin/Insight/model/UserModel.java
@@ -3,6 +3,7 @@ package propensi.Pin.Insight.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.annotations.Type;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
@@ -40,6 +41,7 @@ public class UserModel implements Serializable {
 
     @NotNull
     @Lob
+    @Type(type = "org.hibernate.type.TextType")
     @Column(name="password", nullable = false)
     private String password;
 
