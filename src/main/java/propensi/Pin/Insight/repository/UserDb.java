b73103eed58c2dfc8a86d523581c56867fe0d5d1
Nevil Philia <nevilphiliapramuda@gmail.com>
2021-06-04 07:07:07 +0700

    fix user

--- src/main/java/propensi/Pin/Insight/repository/UserDb.java
+++ src/main/java/propensi/Pin/Insight/repository/UserDb.java
@@ -13,10 +13,8 @@ import java.util.Optional;
 public interface UserDb extends JpaRepository<UserModel, Long> {
 
     Optional<UserModel> findById(Long id);
-//    UserModel findByUsername(String name);
     Optional<UserModel> findByUsername(String username);
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
-    List<UserModel> findAll();
 
 }
